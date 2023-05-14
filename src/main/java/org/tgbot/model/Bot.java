a1a0de568d7d7a1ea812f99146f465489e7afa4e
aaaminov <neverket@gmail.com>
2023-05-14 12:04:48 +0500

    1.05

--- src/main/java/org/tgbot/model/Bot.java
+++ src/main/java/org/tgbot/model/Bot.java
@@ -16,14 +16,17 @@ import org.telegram.telegrambots.meta.api.objects.webapp.WebAppInfo;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.tgbot.db.DbConnection;
 
-
 public class Bot extends TelegramLongPollingBot {
 
     // private String botUsername = "UfanetApprovalApplicationBot";
     // private String botToken = "6230219510:AAEhCAZN7YRDLLBU2cPIKo2v18lg3NL83aw";
     private String botUsername = "NeverKetBot";
     private String botToken = "5848728893:AAENlDxAuca7sJZtKc5rvUy-b00T0ecVOjg";
+
     private String webAppUrl = "https://aaaminov.github.io/UfanetApprovalApplicationBot/web/index.html";
+
+    private String startMessageText = "Привет";
+    private String keyboardButtonText = "Создать заявку";
 
     @Override
     public String getBotUsername() {
@@ -41,60 +44,72 @@ public class Bot extends TelegramLongPollingBot {
         if (!DbConnection.instance.getSubscribersId().contains(from.getId())) {
             DbConnection.instance.saveSubscriber(from);
         }
-
         Message message = update.getMessage();
+
+        // command
         if (message.isCommand()) {
-            switch (message.getText()){
+            switch (message.getText()) {
                 case "/start": {
-                    SendMessage sendMessage = new SendMessage();
-                    sendMessage.setText("hello!");
-                    sendMessage.setChatId(message.getChatId().toString());
-
-                    ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-
-                    WebAppInfo webAppInfo = new WebAppInfo(webAppUrl);
-                    List<KeyboardRow> keyboardRows = new ArrayList<>();
-                    KeyboardRow keyboardRow = new KeyboardRow();
-
-                    KeyboardButton button = new KeyboardButton("WEB app", null, null, null, webAppInfo);
-                    keyboardRow.add(button);
-                    keyboardRows.add( keyboardRow);
-                    replyKeyboardMarkup.setKeyboard(keyboardRows);
-                    replyKeyboardMarkup.setResizeKeyboard(true);
-                    replyKeyboardMarkup.setSelective(true);
-                    sendMessage.setReplyMarkup(replyKeyboardMarkup);
-                    try {
-                        execute(sendMessage);
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
-                    return;
+                    sendStart(message);
+                    break;
                 }
                 case "/approvers": {
                     sendApprovers(message.getFrom().getId());
-                    return;
+                    break;
                 }
             }
-        } else {
-            WebAppData webAppData = message.getWebAppData();
-            if (webAppData != null) {
-                System.out.println(webAppData);
-            }
-    
-            sendText(message.getFrom().getId(), message.getText());
-    
+            return;
         }
+
+        // web app
+        WebAppData webAppData = message.getWebAppData();
+        if (webAppData != null) {
+            System.out.println(webAppData);
+
+            sendText(message.getFrom().getId(), message.getText());
+            return;
+        }
+
+        // echo bot
+        sendText(message.getFrom().getId(), message.getText());
+    }
+
+    private void executeMessage(SendMessage sendMessage) {
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void sendStart(Message message) {
+        SendMessage sm = new SendMessage();
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow keyboardRow = new KeyboardRow();
+
+        WebAppInfo webAppInfo = new WebAppInfo(webAppUrl);
+        KeyboardButton button = new KeyboardButton(keyboardButtonText, null, null, null, webAppInfo);
+
+        keyboardRow.add(button);
+        keyboardRows.add(keyboardRow);
+
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setSelective(true);
+
+        sm.setReplyMarkup(replyKeyboardMarkup);
+        sm.setText(startMessageText);
+        sm.setChatId(message.getChatId().toString());
+
+        executeMessage(sm);
     }
 
     public void sendText(Long userId, String msg) {
         SendMessage sm = SendMessage.builder()
                 .chatId(userId.toString())
                 .text(msg + "").build();
-        try {
-            execute(sm);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
+        executeMessage(sm);
     }
 
     private void sendApprovers(Long userId) {
